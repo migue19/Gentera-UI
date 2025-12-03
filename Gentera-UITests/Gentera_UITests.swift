5b06a291ee6d4a898861b64ca93747cf6463e638
Miguel Mexicano Herrera <miguel.mexicano@ait-business.com>
2025-12-03 13:44:10 -0600

    Initial Commit

---
+++ Gentera-UITests/Gentera_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Gentera_UITests.swift
+//  Gentera-UITests
+//
+//  Created by Miguel.mexicano on 03/12/25.
+//
+
+import Testing
+@testable import Gentera_UI
+
+struct Gentera_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
